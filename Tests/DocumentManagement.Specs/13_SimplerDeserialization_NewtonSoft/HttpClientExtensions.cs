9bb4f2fee573d67aed6ee661565dad9d67a6338f
Dennis Doomen <dennis.doomen@avivasolutions.nl>
2023-02-18 15:23:19 +0100

    Added a System.Text.Json example as version 14.

---
+++ Tests/DocumentManagement.Specs/13_SimplerDeserialization_NewtonSoft/HttpClientExtensions.cs
@@ -0,0 +1,33 @@
+using System.Net.Http;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Newtonsoft.Json;
+
+namespace DocumentManagement.Specs._13_SimplerDeserialization_NewtonSoft
+{
+    internal static class HttpClientExtensions
+    {
+        public static HttpResponseMessageAssertions Should(this HttpResponseMessage response)
+        {
+            return new HttpResponseMessageAssertions(response);
+        }
+    }
+
+    internal class HttpResponseMessageAssertions
+    {
+        private HttpResponseMessage response;
+
+        public HttpResponseMessageAssertions(HttpResponseMessage response)
+        {
+            this.response = response;
+        }
+
+        public async Task BeEquivalentTo<T>(T expectation)
+        {
+            string body = await response.Content.ReadAsStringAsync();
+
+            T actual = JsonConvert.DeserializeAnonymousType(body, expectation);
+            actual.Should().BeEquivalentTo(expectation);
+        }
+    }
+}
